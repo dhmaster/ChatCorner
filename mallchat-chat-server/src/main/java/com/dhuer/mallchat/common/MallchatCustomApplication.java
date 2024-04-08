cd6a548c427025f87d8b222c76cb203a0ee27508
Leo <1509861514@qq.com>
2024-04-08 14:35:42 +0800

    带参二维码接口及用户信息授权接口调用测试

--- mallchat-chat-server/src/main/java/com/dhuer/mallchat/common/MallchatCustomApplication.java
+++ mallchat-chat-server/src/main/java/com/dhuer/mallchat/common/MallchatCustomApplication.java
@@ -1,5 +1,6 @@
 package com.dhuer.mallchat.common;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2024/04/01
  */
 @SpringBootApplication(scanBasePackages = {"com.dhuer.mallchat"})
+@MapperScan({"com.dhuer.mallchat.common.**.mapper"})
 public class MallchatCustomApplication {
 
     public static void main(String[] args) {
